1abd664baec58c0dffdd034c59cb53d79657801c
Chayathorn Lorkanchanakul <gear20056@hotmail.com>
2018-11-07 17:56:34 +0700

    Update While Loop + Show Execerise 1

---
+++ 11-JSControlFlow/WhileLoop/Code.js
@@ -0,0 +1,33 @@
+// Execerise 1
+
+
+var num = 1;
+
+while(num <= 10){
+    console.log(num);
+    num += 2;
+}
+
+
+// Execerise 2
+
+/*
+var num = 1;
+
+while(num <= 20) {
+    if(num % 4 === 0)
+        console.log(num);
+    num++;
+}
+*/
+
+// Execerise 3
+
+/*
+var num = 100;
+
+while(num < 150){
+    console.log(num + 1);
+    num--;
+}
+*/

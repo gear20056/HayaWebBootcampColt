869da2528435172917cd2a94f1478340d2ba7081
Chayathorn Lorkanchanakul <gear20056@hotmail.com>
2018-11-12 04:26:43 +0700

    Update annoy html + add version 2 of annoy js

--- 11-JSControlFlow/WhileLoop/AnnoyOMatic/annoy.js
+++ 11-JSControlFlow/WhileLoop/AnnoyOMatic/annoy.js
@@ -1,14 +1,30 @@
-var answer = prompt("are we there yet?");
+
+// Version 01
+
+// var answer = prompt("are we there yet?");
 
 
-while(answer !== "yes" && answer !== "yeah"){
-    var answer = prompt("are we there yet?");
-}
+// while(answer !== "yes" && answer !== "yeah"){
+//     var answer = prompt("are we there yet?");
+// }
 
-alert("Yay, We made it!");
+// alert("Yay, We made it!");
 
+
+// Version 00
 // if(answer === "yes"){
 //     alert("Yay, We made it!");
 // } else {
 //     var answer = prompt("are we there yet?");
 // }
+
+// Version 02
+
+var answer = prompt("are we there yet?");
+
+
+while(answer.indexOf("yes") === -1){
+    var answer = prompt("are we there yet?");
+}
+
+alert("Yay, We made it!");

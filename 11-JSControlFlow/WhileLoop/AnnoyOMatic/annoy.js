17c7b6c99e71d8da8839d957be2b0302c62bb305
Chayathorn Lorkanchanakul <gear20056@hotmail.com>
2018-11-12 03:58:59 +0700

    Add while loop on AnnoyOMatic

--- 11-JSControlFlow/WhileLoop/AnnoyOMatic/annoy.js
+++ 11-JSControlFlow/WhileLoop/AnnoyOMatic/annoy.js
@@ -1,7 +1,14 @@
 var answer = prompt("are we there yet?");
 
-if(answer === "yes"){
-    alert("Yay, We made it!");
-} else {
+
+while(answer !== "yes"){
     var answer = prompt("are we there yet?");
 }
+
+alert("Yay, We made it!");
+
+// if(answer === "yes"){
+//     alert("Yay, We made it!");
+// } else {
+//     var answer = prompt("are we there yet?");
+// }
